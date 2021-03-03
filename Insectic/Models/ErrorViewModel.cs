ac347f8e67127d107496663d4d9759004094cf47
Darius Dubose <ddubose1@sundevils.asu.edu>
2021-03-03 11:50:06 -0800

    Add project files.

---
+++ Insectic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Insectic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
