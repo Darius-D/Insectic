80aa1bfa9987982994680a244ec16847a87a23d6
Darius <ddubose93@gmail.com>
2021-03-07 00:31:26 -0800

    Commented out Avatar on top nav bar, Fixed issues with VS not importing photos from the wwwroot folder. Corrected CSS syntax due to SCSS.

--- Insectic/wwwroot/js/site.js
+++ Insectic/wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+$(document).ready(function () {
+    all_notes = $("li a");
 
-// Write your JavaScript code.
+    all_notes.on("keyup", function () {
+        note_title = $(this).find("h2").text();
+        note_content = $(this).find("p").text();
+
+        item_key = "list_" + $(this).parent().index();
+
+        data = {
+            title: note_title,
+            content: note_content
+        };
+
+        window.localStorage.setItem(item_key, JSON.stringify(data));
+    });
+
+    all_notes.each(function (index) {
+        data = JSON.parse(window.localStorage.getItem("list_" + index));
+
+        if (data !== null) {
+            note_title = data.title;
+            note_content = data.content;
+
+            $(this).find("h2").text(note_title);
+            $(this).find("p").text(note_content);
+        }
+    });
+});
