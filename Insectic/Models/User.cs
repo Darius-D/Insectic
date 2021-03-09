dfe29004e91f69c5da06df8da61b8b7ce70b8cc8
Darius <ddubose93@gmail.com>
2021-03-09 00:28:53 -0800

    Created User Model to start working on User login page

---
+++ Insectic/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Insectic.Models
+{
+    public class User
+    {
+        public string UserId { get; private set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public string UserPassword { get; private set; }
+
+        public string? ResourceGroup { get; set; }
+
+        public int UserRoles { get; set; }
+
+
+
+        public User(string password, string resourceGroup)
+        {
+            ResourceGroup = resourceGroup;
+            UserPassword = password;
+        }
+
+
+    }
+}
