2f8399a9b899facfccff1be6109d8f62c705bed8
Darius <ddubose93@gmail.com>
2021-03-14 12:14:26 -0700

    Created a Comment Model and Comment Repository for View Ticket view that will be completed prior to 20 March, 2021.

---
+++ Insectic/Models/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Insectic.Models
+{
+    public class Comment
+    {
+        private static int count = 100;
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public int TicketId { get; set; }
+        public string UserId { get; set; }
+        public int CommentId { get; }
+        public DateTime CommentDateTime { get; set; }
+
+        public Comment()
+        {
+            CommentId = count;
+            count++;
+        }
+    }
+}
---
+++ Insectic/Models/CommentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Insectic.Models
+{
+    public class CommentRepository
+    {
+        private static List<Comment> comments = new List<Comment>();
+
+        public static IEnumerable<Comment> Comments => comments;
+
+        public static void AddUser(Comment comment)
+        {
+            comments.Add(comment);
+        }
+    }
+}
