3cc7b388ea3fd5307ccb510af788bb79d4ad235c
Darius <ddubose93@gmail.com>
2021-03-31 20:45:28 -0700

    Added new test

--- InsecticApiTests/Tickets/TicketControllerTest.cs
+++ InsecticApiTests/Tickets/TicketControllerTest.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace InsecticApiTests.Tickets
 {
     class TicketControllerTest
     {
+        [Fact]
+        public async Task GetTicket_ShouldReturnSingle_ByTicketId()
+        {
+
+
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
     }
 }
